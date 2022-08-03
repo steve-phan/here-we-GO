6e07344c6b6286bdae646b0a177a9a77431bbf7e
Steve Phan <phanhaingoc@gmail.com>
2022-08-03 16:40:01 +0200

    remove submodule hello

---
+++ Hello_/go.mod
@@ -0,0 +1,7 @@
+module bookable24.de/hello
+
+go 1.19
+
+replace bookable24.de/greetings => ../greetings
+
+require bookable24.de/greetings v0.0.0-00010101000000-000000000000
---
+++ Hello_/hello.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"bookable24.de/greetings" // TODO : $GOPATH => BrokenImport
+)
+
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("Greetings:   ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Teddy", "Luna", "Lily"}
+
+	// Request greeting messages for the names slice
+	messages, err := greetings.Hellos(names)
+
+	// Request a greeting message.
+	// message, err := greetings.Hello("Luna")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned
+	// message | messages to the console.
+	// fmt.Println(message)
+	fmt.Println(messages)
+}
