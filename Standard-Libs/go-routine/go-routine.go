c0ba964bb55e83a68d3c41be78e3d4e94123a488
Steve Phan <phanhaingoc@gmail.com>
2022-08-09 22:31:33 +0200

    go routine

---
+++ Standard-Libs/go-routine/go-routine.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(2) // assign a number(N) of wg tasks
+
+	go func() {
+		countLeng("Hello World")
+		wg.Done() // it means N--
+	}()
+
+	go func() {
+		countLeng("GoodBye")
+		wg.Done() // it means N-- = 0
+	}()
+
+	wg.Wait() // When N == 0 (finish and go next)
+	fmt.Println("Count done")
+
+}
+
+func countLeng(str string) {
+
+	for i := 0; i < len(str); i++ {
+		fmt.Println(string(str[i]))
+	}
+
+}
